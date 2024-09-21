9387628ae4751a54d1d332a3ac7e66002e3c6d58
ugnspa <125737106+ugnspa@users.noreply.github.com>
2024-09-21 20:44:35 +0300

    added Enums

---
+++ WrestlingTournamentSystem.DataAccess/Data/WrestlingTournamentSystemDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WrestlingTournamentSystem.DataAccess.Entities;
+
+namespace WrestlingTournamentSystem.DataAccess.Data
+{
+    public class WrestlingTournamentSystemDbContext : DbContext
+    {
+        public WrestlingTournamentSystemDbContext(DbContextOptions<WrestlingTournamentSystemDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TournamentStatus> TournamentStatuses { get; set; }
+        public DbSet<WrestlingStyle> WrestlingStyles { get; set; }
+        public DbSet<TournamentWeightCategoryStatus> TournamentWeightCategoryStatuses { get; set; }
+    }
+}
---
+++ WrestlingTournamentSystem.DataAccess/Entities/TournamentWeightCategoryStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WrestlingTournamentSystem.DataAccess.Entities
+{
+    public class TournamentWeightCategoryStatus
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(12)]
+        public string Name { get; set; } = null!;
+    }
+}
