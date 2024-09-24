0348bb1274cd50932e5f190158918d1c5ee2a8ce
ugnspa <125737106+ugnspa@users.noreply.github.com>
2024-09-24 23:44:30 +0300

    Added mapper

---
+++ WrestlingTournamentSystem.DataAccess/Mappers/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using WrestlingTournamentSystem.DataAccess.DTO.Tournament;
+using WrestlingTournamentSystem.DataAccess.Entities;
+
+namespace WrestlingTournamentSystem.DataAccess.Mappers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Tournament, TournamentReadDTO>()
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.TournamentStatus.Name));
+            CreateMap<TournamentCreateDTO, Tournament>();
+            CreateMap<TournamentUpdateDTO, Tournament>();
+        }
+    }
+}
